0ecf502fbdc72527e128414fd0b3fb38579021ef
Leandro Burgos <burgosleandro.c@gmail.com>
2023-02-20 17:13:44 -0300

    update some file

--- item/models.py
+++ item/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
 
     def __str__(self):
-      return self.name
+        return self.name
     
 class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
@@ -20,3 +20,6 @@ class Item(models.Model):
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 def index(request):
-    return render(request, 'marketplace/index.html')
+    items = Item.objects.filter(is_sold = False)[0:6]
+    categorias = Category.objects.all()
+    return render(request, 'marketplace/index.html', {
+        'categorias': categorias,
+        'items': items,
+    })
 
 def contact(request):
     return render(request, 'marketplace/contact.html')
