8031223c4dccae31572cce96e880fe8a320f3492
Leandro Burgos <burgosleandro.c@gmail.com>
2023-03-02 21:08:51 -0300

    add form login and html login

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Nombre de usuario',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingrese su contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -9,5 +20,21 @@ class SignupForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': ''
+        'placeholder': 'Nombre de usuario',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Dirección de correo electrónico',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingrese su contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repita su contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl',
     }))
