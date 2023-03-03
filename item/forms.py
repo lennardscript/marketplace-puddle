86f63875e0ee72b80448107613f35871dc7ba687
Leandro Burgos <burgosleandro.c@gmail.com>
2023-03-02 21:43:05 -0300

    add form in section new items

---
+++ item/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import Item
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            })
+        }
